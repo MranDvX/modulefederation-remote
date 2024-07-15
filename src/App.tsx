3ad09bd3d0db8d08232cecaa8b255a67597edf66
Francisco Mancuello Vazquez <franmavazq@gmail.com>
2024-07-15 19:52:15 -0400

    test: :fire: test for jamilton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import List from "./components/List";
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+  // const [count, setCount] = useState<number>(0);
 
   return (
     <>
-      <Input value={count} onChange={setCount} onSubmit={console.log} />
+      {/* <Input value={count} onChange={setCount} onSubmit={console.log} /> */}
+      <Input />
       <List items={["Learn React", "Learn Vite", "Make an awesome app"]} />
     </>
   );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,27 +1,33 @@
-interface InputProps {
-  value: number;
-  onChange: (newValue: number) => void;
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-}
+// interface InputProps {
+//   value: number;
+//   onChange: (newValue: number) => void;
+//   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+// }
 
-const Input: React.FC<InputProps> = ({ value, onChange, onSubmit }) => {
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        onSubmit(e);
-      }}
-    >
-      <div className="flex-row">
-        <input
-          type="text"
-          value={value.toString()}
-          onChange={(e) => onChange(Number(e.target.value))}
-        />
-        <button type="submit">Add</button>
-      </div>
-    </form>
-  );
+// const Input: React.FC<InputProps> = ({ value, onChange, onSubmit }) => {
+//   return (
+//     <form
+//       onSubmit={(e) => {
+//         e.preventDefault();
+//         onSubmit(e);
+//       }}
+//     >
+//       <div className="flex-row">
+//         <input
+//           type="text"
+//           value={value.toString()}
+//           onChange={(e) => onChange(Number(e.target.value))}
+//         />
+//         <button type="submit">Add</button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default Input;
+
+const Input: React.FC = () => {
+  return <h1>hi</h1>;
 };
 
 export default Input;
